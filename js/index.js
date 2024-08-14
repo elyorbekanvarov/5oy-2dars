d9f9815d488c452994974f5fee1f3ba9ee4fcd15
elyorbekanvarov <e0275king@gmail.com>
2024-08-14 09:15:04 +0500

    2-dars

---
+++ js/index.js
@@ -0,0 +1,102 @@
+// function User(name, age, phone, email) {
+//     this.name = name;
+//     this.age = age;
+//     this.phone = phone;
+//     this.email = email;
+// }
+// let john = new User("John", 45, '+43123', "john@gmail.com");
+// console.log(john);
+
+// let user = {
+//     name: 'John',
+//     age: 45,
+//     address: {
+//         city: "Tashkent",
+//     }
+// }
+// console.log(user.address?.city);
+
+// 2 - masala
+
+// function boshXarfiniKattaQil(arg) {
+//     let res = arg.map(function(value) {
+//         return value[0].toUpperCase() + value.slice(1);
+//     })
+//     return res;
+// }
+// let arr = ["salom", "yaxshimisizlar"];
+// console.log(boshXarfiniKattaQil(arr));
+
+// 3 - masala
+
+// function raqamOl(str) {
+//     for (const iterator of str) {
+//         if (iterator < '0' || iterator > '9') {
+//             res.push(iterator);
+//         }
+//     }
+//     return res.join();
+// }
+// let res = [];
+// let test = "sa2lom1";
+// console.log(raqamOl(test));
+
+// 4 - masala
+
+// function stringSave(arg) {
+//     for (const iterator of arg) {
+//         if (typeof iterator === "string") {
+//             res.push(iterator);
+//         }
+//     }
+//     return res;
+// }
+// let res = [];
+// let arr = ["salom", 2, true, undefined, "yaxshimisizlar"];
+// console.log(stringSave(arr));
+
+// 5 - masala
+
+// function countWord(str) {
+//     let res = str.split(' ').length;
+//     return res;
+// }
+// let test = "salom dunyo";
+// console.log(countWord(test));
+
+// 6 - masala
+
+// function unliXarfOl(str) {
+//     for (const iterator of str) {
+//         if (iterator == "s" || iterator == "l" || iterator == "m" || iterator == "y" || iterator == "x" || iterator == 'z' || iterator == "k" || iterator == "b" || iterator == "r" && iterator == "S" || iterator == "I" || iterator == "M" || iterator == "Y" || iterator == "X" || iterator == 'Z' || iterator == "K" || iterator == "B" || iterator == "R") {
+//             res.push(iterator);
+//         }
+//     }
+//     return res.join('');
+// }
+// let res = [];
+// let test = "Assalomu Alaykum Yaxshimisizlar Brodarlarim";
+// console.log(unliXarfOl(test));
+
+// 7 - masala
+
+// function strLength(arg) {
+//     let res = '';
+//     for (const iterator of arg) {
+//         if (iterator.length > res.length && typeof iterator == "string") {
+//             res = iterator;
+//         }
+//     }
+//     return res;
+// }
+// let arr = ["salom", "javascript"];
+// console.log(strLength(arr));
+
+// 8 - masala
+
+// function strReverse(str) {
+//     let res = str.split('').reverse().join('');
+//     return res;
+// }
+// let test = "salom";
+// console.log(strReverse(test));
